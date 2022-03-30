c2d108fdbc76bdfe4c8a9d2ffbfd3c33361855a4
nujgnoej <junmo8492@naver.com>
2022-03-30 12:23:14 +0900

    0330_1223 데이터베이스연습(수정)

---
+++ src/t2_DBTest/DBTest.java
@@ -0,0 +1,74 @@
+package t2_DBTest;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+// DAO(Data Access Object)
+public class DBTest {
+	Connection conn = null;
+	Statement stmt = null;
+	ResultSet rs = null;
+	
+	String sql = "";
+	
+	public DBTest() {
+		try {
+			// 1. JDBC 드라이버 검색
+			Class.forName("com.mysql.jdbc.Driver");
+			
+			// 2. 데이터베이스 연결
+			String url = "jdbc:mysql://localhost:3306/javagreen";
+			String user = "root";
+			String password = "1234";
+			conn = DriverManager.getConnection(url, user, password);
+		} catch (ClassNotFoundException e) {
+			System.out.println("드라이버 검색 실패~~~");
+			System.exit(0);
+		} catch (SQLException e) {
+			System.out.println("데이터베이스 연동 실패~~~");
+			System.exit(0);
+		}
+	}
+
+	// 데이터베이스안의 테이블 검색
+	public void searchTest(String name) {
+		try {
+			stmt = conn.createStatement();
+			sql = "select * from aaa where name='"+name+"'";
+			rs = stmt.executeQuery(sql);
+			
+			if(rs.next()) {
+				//System.out.println("홍길동을 찾았다.");
+				name = rs.getString("name");
+				int age = rs.getInt("age");
+				String gender = rs.getString("gender");
+				String joinday = rs.getString("joinday");
+				System.out.println("========================");
+				System.out.println("성명 : " + name);
+				System.out.println("나이 : " + age);
+				System.out.println("성별 : " + gender);
+				System.out.println("가입일 : " + joinday);
+				System.out.println("========================");
+			}
+			else {
+				System.out.println("========================");
+				System.out.println(name + "을(를) 찾지 못했습니다.");
+				System.out.println("========================");
+			}
+			
+		} catch (SQLException e) {
+			System.out.println("SQL오류" + e.getMessage());
+		}
+	}
+	
+  // 데이터베이스객체 Close 
+	public void dbClose() {
+		try {
+			conn.close();
+			System.out.println("DB를 닫았습니다.");
+		} catch (Exception e) {}
+	}
+}
---
+++ src/t2_DBTest/DBTestRun.java
@@ -0,0 +1,26 @@
+package t2_DBTest;
+
+import java.util.Scanner;
+
+public class DBTestRun {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		// DB연동후 작업처리...
+		DBTest dbTest = new DBTest();
+		
+		System.out.println("DB 처리 작업중입니다.....");
+
+		// DB검색작업(이름 검색)
+		String name;
+		System.out.print("찾고자 하는 성명을 입력하세요? ");
+		name = scanner.next();
+		dbTest.searchTest(name);
+		
+		System.out.println("DB 처리 작업끝..입니다.....");
+		
+		// DB Close처리
+		dbTest.dbClose();
+		
+		scanner.close();
+	}
+}
